d457563af9fe23ac7b333d08da22d65d8d3fc604
Noah Cardoza <noahcardoza@gmail.com>
2018-07-05 09:42:50 -0700

    Updated algorithm

--- python/d01/ex01/main.py
+++ python/d01/ex01/main.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import time
+from math import floor
 
 
 # press ctrl-c to quit
@@ -8,9 +9,9 @@ import time
 # get your token at http://51.38.128.213:3000/
 token = ''
 
-percentageThreshold = 1 / 100 # 10%
-lastSellProfit = 0
-lastPurchaseCost = 0
+percentageThreshold = 5 / 100 # 10%
+lastSellPrice = 0
+lastPurchasePrice = 0
 
 s = requests.Session()
 s.headers.update({
@@ -18,7 +19,8 @@ s.headers.update({
 })
 
 def url(path):
-    return 'http://51.38.128.213:3000' + path
+    # 'http://51.38.128.213:3000'
+    return 'http://localhost:3000' + path
 
 def getPrice():
     return s.get(url('/price')).json()['value']
@@ -30,14 +32,14 @@ def getBtc():
     return s.get(url('/btc')).json()['value']
 
 def buyBtc(amount):
-    print(s.get(url('/buy'), params={
+    return s.get(url('/buy'), params={
         'amount': amount
-    }).json()['message'])
+    })
 
 def sellBtc(amount):
-    print(s.get(url('/sell'), params={
+    return s.get(url('/sell'), params={
         'amount': amount
-    }).json()['message'])
+    })
 
 balance = getBalance()
 btc = getBtc()
@@ -46,25 +48,61 @@ print ('Balance:', balance)
 print ('BTC:', btc)
 
 if btc:
+    lastSellPrice = getPrice()
     sellBtc(btc)
-    lastSellProfit = getPrice()
+    print ('Selling your inital {} BTC for ${}.'.format(btc, btc * lastSellPrice))
+    balance += btc * lastSellPrice
     btc = 0
 
 while True:
     price = getPrice()
-    if not btc:
-        btcToBuy = round(price / balance, 2)
-        cost = btcToBuy * price
-        if not lastSellProfit or cost * (1 + percentageThreshold) < lastSellProfit:
-            buyBtc(btcToBuy)
-            balance -= cost
-            btc += btcToBuy
-            lastPurchaseCost = cost # 5000
-    else:
+    if not btc: # time to buy btc
+        btcToBuy = balance / price
+        cost = balance
+        if lastSellPrice:
+            print ('Could buy at a gain of {}%'.format(round(100 - (price / lastSellPrice * 100), 2)))
+        if not lastSellPrice or price * (1 + percentageThreshold) < lastSellPrice:
+            if buyBtc(btcToBuy).status_code == 200:
+                if lastSellPrice:
+                    print ('Bought {amount} BTC for {cost} at {price} per BTC. Bought for {percentage}% than it was sold for.'.format(
+                        amount=btcToBuy,
+                        cost=cost,
+                        price=price,
+                        percentage=round(100 - (price / lastSellPrice * 100), 2)
+                    ))
+                else:
+                    print ('Made first purchase of {amount} BTC for {cost} at {price} per BTC.'.format(
+                        amount=btcToBuy,
+                        cost=cost,
+                        price=price,
+                    ))
+                balance -= cost
+                btc += btcToBuy
+                lastPurchasePrice = price
+            else:
+                print ('Error trying to buy BTC.')
+    else: # time to sell btc
         profit = btc * price
-        if profit * (1 - percentageThreshold) > lastPurchaseCost:
-            sellBtc(btc)
-            balance += profit
-            btc = 0
-            lastSellProfit = profit
+        if lastPurchasePrice:
+            print ('Could sell at a gain of {}%'.format(round(100 - (lastPurchasePrice / price * 100))))
+        if price * (1 - percentageThreshold) > lastPurchasePrice:
+            if sellBtc(btc).status_code == 200:
+                if lastPurchasePrice:
+                    print ('Sold {amount} BTC for {profit} at {price} per BTC. Sold for {percentage}% more than it was purchased for.'.format(
+                        amount=btc,
+                        profit=profit,
+                        price=price,
+                        percentage=round(100 - (lastPurchasePrice / price * 100))
+                    ))
+                else:
+                    print ('Made first sale of {amount} BTC for {profit} at {price} per BTC.'.format(
+                        amount=btc,
+                        profit=profit,
+                        price=price
+                    ))
+                balance += profit
+                btc = 0
+                lastSellPrice = price
+            else:
+                print ('Error trying to sell BTC.')
     time.sleep(1)
